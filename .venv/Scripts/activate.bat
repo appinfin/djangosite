3e3de157f4e6edb82cb7d4c586ec9c4d120e83bf
appinfin <desktop.developer@yandex.ru>
2024-03-04 12:00:11 +0500

    activate_repair

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\my_app\Django_Python_MyFirstApp\djangosite\.venv
+set VIRTUAL_ENV=E:\My_app\Django_Python_MyFirstApp\djangosite\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
