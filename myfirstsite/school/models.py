2b0097b6aaf35a3b63be76b19617f36a7f26b80a
appinfin <desktop.developer@yandex.ru>
2024-03-04 17:36:45 +0500

    migration_0002

--- myfirstsite/school/models.py
+++ myfirstsite/school/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 # # Create your models here.
-# class Person(models.Model):
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
-#     telephone = models.IntegerField()
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    telephone = models.CharField(max_length=11)
+    telephone = models.IntegerField()
